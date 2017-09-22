c2172cab3986545a8515c70c7b3a0c3a8cd1f544
Matt Lee <matthewlee07@gmail.com>
2017-09-22 11:56:47 -0400

    server connected

--- config.js
+++ config.js
@@ -5,3 +5,4 @@ exports.DATABASE_URL =
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+console.log(exports);

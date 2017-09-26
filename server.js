3706d0fbb9256b70fab02a6a20a697d34f5564de
Matt Lee <matthewlee07@gmail.com>
2017-09-26 11:19:51 -0400

    changed server.js from mongo to postgres

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
 const passport = require('passport');
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+const bodyParser = require('body-parser');
+const {DATABASE, PORT} = reuqire('./config');
+const knex = require('knex')(DATABASE);
+
 require('dotenv').config();
 
 const { router: usersRouter } = require('./users');
@@ -14,13 +17,14 @@ const { router: authRouter, basicStrategy, jwtStrategy } = require('./auth');
 const { PORT, DATABASE_URL } = require('./config');
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
-const app = express();
+
 
 passport.use(basicStrategy);
 passport.use(jwtStrategy);
 
 app.use(morgan('common', { skip: () => process.env.NODE_ENV === 'test' }));
 app.use(cors());
+app.use(bodyParser.jason());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,29 +40,26 @@ app.use('*', (req, res) => {
 });
 
 let server;
-function runServer() {
+function runServer(database = DATABASE, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(DATABASE_URL, { useMongoClient: true }, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app
-        .listen(PORT, () => {
-          console.log(`Your app is listening on port ${PORT}`);
-          resolve();
-        })
-        .on('error', err => {
-          mongoose.disconnect();
-          reject(err);
-        });
-    });
+    try {
+      knex = require('knex')(database);
+      server = app.listen(port, () => {
+        console.info(`App listening on port ${server.address().port}`);
+        resolve();
+      });
+    }
+    catch (err) {
+      console.error(`Can't start server: ${err}`);
+      reject(err);
+    }
   });
 }
 
 function closeServer() {
-  return mongoose.disconnect().then(() => {
+  return knex.destroy().then(() => {
     return new Promise((resolve, reject) => {
-      console.log('Closing server');
+      console.log('Closing servers');
       server.close(err => {
         if (err) {
           return reject(err);

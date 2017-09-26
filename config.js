2f4ac69743c732aee458ed6450c46865a72e85ed
Matt Lee <matthewlee07@gmail.com>
2017-09-26 11:23:58 -0400

    changed mongo to postgresql in test folders

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 exports.DATABASE_URL =
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    'mongodb://localhost/jwt-auth-demo';
+    'postgresql://localhost/jwt-auth-demo';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 
